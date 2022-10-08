3a6c9927ce880d7a5afa8bf869a039fe3d038773
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-08 14:59:59 +0900

    test6

--- static/js/admin.js
+++ static/js/admin.js
@@ -90,7 +90,7 @@ async function main() {
     const filename = await getAPI('https://project-research.azurewebsites.net/api/database/create_backup');
     console.log(filename);
     const backupButton = document.querySelector('#backup');
-    backupButton.href = `https://project-research.azurewebsites.net/backup/${filename}`;
+    backupButton.href = `./backup/${filename}`;
 }
 main();
 
